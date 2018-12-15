d509390f06a54e6ec7851d6bef4edfd4f1b66715
Abirami33 <38695790+Abirami33@users.noreply.github.com>
2018-12-15 12:40:12 +0530

    Add files via upload

---
+++ spy.py
@@ -0,0 +1,17 @@
+#module for add sub mul div functions
+
+def add(a,b):
+	x=a+b
+	print("Result is",x)
+
+def sub(a,b):
+	x=a-b
+	print("Result is",x)
+	
+def mul(a,b):
+	x=a*b
+	print("Result is",x)
+
+def div(a,b):
+	x=a/b
+	print("Result is",x)
